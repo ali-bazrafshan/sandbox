af5e951b4613dfe516c6e2f168022c9c08635e3a
Ali Bazrafshan <alibazrafshan1707@gmail.com>
2026-02-18 13:49:23 +0330

    dotnet-redis: Use hash maps

--- dotnet/demos/src/Redis/Program.cs
+++ dotnet/demos/src/Redis/Program.cs
@@ -3,6 +3,15 @@
 var muxer = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
 var db = muxer.GetDatabase();
 
-await db.StringSetAsync("testKey", "testValue");
-string? result = await db.StringGetAsync("testKey");
+// String
+await db.StringSetAsync("user:1", "Ali Bazrafshan");
+string? result = await db.StringGetAsync("user:1");
 Console.WriteLine(result);
+
+// Hash map
+var map = new HashEntry[]
+{
+    new("first-name", "Ali"),
+    new("last-name", "Bazrafshan")
+};
+await db.HashSetAsync("user:2", map);
